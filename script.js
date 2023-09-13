64655155c1cb1776a8394a510e3f2d34198da9ad
youlookweird <124681293+youlookweird@users.noreply.github.com>
2023-09-13 17:32:39 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// JavaScript to handle the popup
+document.getElementById("show-form").addEventListener("click", function() {
+    document.getElementById("contact-popup").style.display = "block";
+});
+
+document.getElementById("close-form").addEventListener("click", function() {
+    document.getElementById("contact-popup").style.display = "none";
+});
